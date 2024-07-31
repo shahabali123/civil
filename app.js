749448f55cc84c8c6b3531cd37a11e791b48cc60
shahabali123 <shahab.mughal.180@gmail.com>
2024-07-31 21:21:43 +0400

    added mongo store for production environment

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-    }
+    },
+    store: store,
 }
 app.use(session(sessionOptions));
 app.use(flash());
